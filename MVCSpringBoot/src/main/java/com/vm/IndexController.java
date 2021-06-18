ca512ebb1071c335e87411da9a09abe4498951dc
Sai Teja <Sai Teja@DESKTOP-N2VQBHK>
2021-06-18 13:02:44 +0530

    initial commit

---
+++ MVCSpringBoot/src/main/java/com/vm/IndexController.java
@@ -0,0 +1,16 @@
+package com.vm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/books/{name}/{course}")
+	public String hello(@PathVariable("name") String name,@PathVariable("course") String course)
+	{
+		return "hello"+"\t" +name+"\t" +course;
+	}
+}
---
+++ MVCSpringBoot/src/test/java/com/vm/MvcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
